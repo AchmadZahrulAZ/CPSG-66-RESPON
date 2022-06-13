7de7c360113c547ef03a6810f3108064ef321c16
AchmadZahrulAZ <achmadzahrul01@gmail.com>
2022-06-13 16:15:13 +0700

    Update dashboard staff
    Banyak beberapa bug tampilan yang sudah di solve

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,12 +1,14 @@
-import StaffDashboard from '../views/pages/staff-dashboard';
-import StaffDataAntrian from '../views/pages/staff-dataAntrian';
-import StaffPengaturanAkun from '../views/pages/staff-pengaturanAkun';
+import StaffHome from '../views/pages/staff-home';
+import StaffDataAntrian from '../views/pages/staff-data-antrian';
+import StaffFaq from '../views/pages/staff-faq';
+import StaffInformasiPuskesmas from '../views/pages/staff-informasi-puskesmas';
 
 const routes = {
-  '/': StaffDashboard,
-  '/staffdashboard': StaffDashboard,
-  '/staffantrian': StaffDataAntrian,
-  '/staffakun': StaffPengaturanAkun,
+  '/': StaffHome,
+  '/staffhome': StaffHome,
+  '/staffdataantrian': StaffDataAntrian,
+  '/staffinformasipuskesmas': StaffInformasiPuskesmas,
+  '/stafffaq': StaffFaq,
 };
 
 export default routes;
---
+++ src/scripts/views/pages/staff-data-antrian.js
@@ -0,0 +1,18 @@
+const StaffDataAntrian = {
+  async render() {
+    return `
+      <div class="p-4 mb-4 bg-light rounded-3">
+          <div class="container-fluid py-5 text-center">
+              <h1 class="display-6 fw-bold">Data Antrian</h1>
+              <p class="fs-4">lorem</p>
+          </div>
+      </div>
+      `;
+  },
+
+  async afterRender() {
+    // test
+  },
+};
+
+export default StaffDataAntrian;

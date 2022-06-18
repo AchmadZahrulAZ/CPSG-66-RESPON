beeb3e61ebd67fe1688b5f75953a7938de47efc8
ahmadWahyu7 <ahmadwahyupratama5@gmail.com>
2022-06-18 10:38:12 +0700

    menampilkan data home
    - sementara pake button yes or no
    - tambah poli belum dibuat logika di js nya

--- src/scripts/views/pages/staff-home.js
+++ src/scripts/views/pages/staff-home.js
@@ -1,3 +1,5 @@
+import { puskesmasNoData, puskesmasWithData } from '../templates/template-creator';
+
 const StaffHome = {
   async render() {
     return `
@@ -7,12 +9,40 @@ const StaffHome = {
             <p class="fs-4">Selamat datang di dashboard staff RESPON(Reservasi Puskesmas Online)</p>
         </div>
     </div>
-    
+    <button id="ya" class="btn btn-primary mt-3">ya</button>
+    <button id="tidak" class="btn btn-danger mt-3">tidak</button>
+    <div id="puskesmas-data" class="contoh"></div>
     `;
   },
 
   async afterRender() {
-    // test
+    /* 
+    buat variable yang menampung data puskesmas
+    buat percabangan if apakah data tersedia
+    - jika ya maka tampilkan template data puskesmas
+    - jika tidak maka tampilkan template data kosong
+
+    const puskesmasData = await .... //ambil data dari database
+    const puskesmasContainer  = document.querySelector('#puskesmas-data');
+    if (puskesmasData.length > 0 ??) { //kondisi jika data puskesmas ada
+      puskesmasContainer.innerHTML += .... // tampilan yang ada datanya
+    } else {
+      puskesmasContainer.innerHTML += .... // tampilan belum ada data
+    }
+    */
+    const puskesmasContainer = document.querySelector('#puskesmas-data');
+    // puskesmasContainer.innerHTML += puskesmasNoData;
+    // puskesmasContainer.innerHTML += puskesmasWithData;
+    const yes = document.querySelector('#ya');
+    const no = document.querySelector('#tidak');
+    yes.addEventListener('click', () => {
+      puskesmasContainer.innerHTML = '';
+      puskesmasContainer.innerHTML += puskesmasWithData;
+    });
+    no.addEventListener('click', () => {
+      puskesmasContainer.innerHTML = '';
+      puskesmasContainer.innerHTML += puskesmasNoData;
+    });
   },
 };
 

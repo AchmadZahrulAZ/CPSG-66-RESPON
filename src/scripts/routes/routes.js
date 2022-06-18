9ba85555c655395ee0e7de7f423ea650809a2a04
ahmadWahyu7 <ahmadwahyupratama5@gmail.com>
2022-06-18 10:38:12 +0700

    menampilkan data home
    - sementara pake button yes or no
    - tambah poli belum dibuat logika di js nya

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,6 +2,7 @@ import StaffHome from '../views/pages/staff-home';
 import StaffDataAntrian from '../views/pages/staff-data-antrian';
 import StaffFaq from '../views/pages/staff-faq';
 import StaffInformasiPuskesmas from '../views/pages/staff-informasi-puskesmas';
+import IsiDataPuskesmas from '../views/pages/staff-isi-data-puskesmas';
 
 const routes = {
   '/': StaffHome,
@@ -9,6 +10,7 @@ const routes = {
   '/staffdataantrian': StaffDataAntrian,
   '/staffinformasipuskesmas': StaffInformasiPuskesmas,
   '/stafffaq': StaffFaq,
+  '/staffisidatapuskesmas': IsiDataPuskesmas,
 };
 
 export default routes;

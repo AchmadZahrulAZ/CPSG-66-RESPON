6bff636c098a35c1dabd5f824a39eedae9cec2cd
AchmadZahrulAZ <achmadzahrul01@gmail.com>
2022-07-02 11:07:49 +0700

    Push commit 401-admin-home

--- src/scripts/views/pages/admin-data-admin.js
+++ src/scripts/views/pages/admin-data-admin.js
@@ -1,17 +1,97 @@
+import axios from 'axios';
+
 const AdminDataAdmin = {
   async render() {
     return `
-      <div class="p-4 mb-4 bg-light rounded-3">
-          <div class="container-fluid py-5 text-center">
-              <h1 class="display-6 fw-bold">Data Admin</h1>
-              <p class="fs-4">test</p>
+      <div class="container">
+        <table class="table table-light table-bordered table-hover">
+          <thead class="table-warning">
+            <tr>
+              <th scope="col" class="text-center">ID</th>
+              <th scope="col" class="text-center">NAMA</th>
+              <th scope="col" class="text-center">NIK</th>
+              <th scope="col" class="text-center">EMAIL</th>
+              <th scope="col" class="text-center"></th>
+            </tr>
+          </thead>
+          <tbody id="tes">
+
+          </tbody>
+        </table>
+      </div>
+
+      <div class="container shadow bg-light p-3 my-5 rounded-4">
+        <h3 class="text-center mt-3 mb-5">TAMBAH DATA ADMIN</h3>
+          <form autocomplete="off" id="isi-data-admin">
+              <div class="mb-3 row px-4">
+                  <label for="Nama" class="col-sm-5 col-form-label">NAMA</label>
+                  <div class="col-sm-7">
+                  <input type="text" class="form-control" name="nama" id="nama">
+                  </div>
+              </div>
+              <div class="mb-3 row px-4">
+                  <label for="Nik" class="col-sm-5 col-form-label">NIK</label>
+                  <div class="col-sm-7">
+                  <input type="text" class="form-control" name="nik" id="nik">
+                  </div>
+              </div>
+              <div class="mb-3 row px-4">
+                  <label for="Email" class="col-sm-5 col-form-label">EMAIL</label>
+                  <div class="col-sm-7">
+                  <input type="text" class="form-control" name="email" id="email">
+                  </div>
+              </div>
+              <div class="d-flex justify-content-center">
+                  <button type="submit" value="submit" class="btn btn-primary mt-3 px-5 fw-bold">Daftar</button>
+              </div>
+          </form>
+      </div>
+
+      <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
           </div>
+        </div>
       </div>
       `;
   },
 
   async afterRender() {
-    // test
+    // ini menampilkan seluruh data API
+    const res = await axios('https://respon-backend.vercel.app/user/list');
+    console.log(res);
+    console.log(res.data[0]);
+    let datalist = '';
+    res.data.forEach((data) => {
+      datalist += `
+      <tbody>
+        <tr>
+          <th scope="row" class="text-center">${data.id}</th>
+          <td class="text-center">${data.nama}</td>
+          <td class="text-center">${data.nik}</td>
+          <td class="text-center">${data.email}</td>
+          <td class="d-flex justify-content-center">
+            <button type="button" onClick="" class="btn btn-warning" data-toggle="modal" data-target="#exampleModalCenter">Edit</button> 
+            <button type="button" class="btn btn-danger">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    `;
+      document.querySelector('#tes').innerHTML = datalist;
+    });
   },
 };
 
--- src/scripts/views/pages/admin-data-pasien.js
+++ src/scripts/views/pages/admin-data-pasien.js
@@ -1,17 +1,47 @@
+import axios from 'axios';
+
 const AdminDataPasien = {
   async render() {
     return `
-      <div class="p-4 mb-4 bg-light rounded-3">
-          <div class="container-fluid py-5 text-center">
-              <h1 class="display-6 fw-bold">Data Pasien</h1>
-              <p class="fs-4">lorem</p>
-          </div>
-      </div>
+    <div class="container">
+    <table class="table table-light table-bordered table-hover">
+      <thead class="table-warning">
+        <tr>
+          <th scope="col" class="text-center">Nomor Antrian</th>
+          <th scope="col" class="text-center">Nama</th>
+          <th scope="col" class="text-center">Poli</th>
+          <th scope="col" class="text-center">Jenis</th>
+          <th scope="col" class="text-center">Hapus Antrian</th>
+        </tr>
+      </thead>
+      <tbody id="tes">
+
+      </tbody>
+    </table>
+  </div>
       `;
   },
 
   async afterRender() {
-    // test
+    const res = await axios('https://respon-backend.vercel.app/pasien/list');
+    let datalist = '';
+    res.data.forEach((data) => {
+      datalist += `
+      <tbody>
+        <tr>
+          <th scope="row" class="text-center">${data.id}</th>
+          <td class="text-center">${data.name}</td>
+          <td class="text-center">${data.poli}</td>
+          <td class="text-center">${data.tax}</td>
+          <td class="d-flex justify-content-center">
+            <button type="button" class="btn btn-warning">Edit</button>
+            <button type="button" class="btn btn-danger" id="delete">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+      `;
+      document.querySelector('#tes').innerHTML = datalist;
+    });
   },
 };
 
